9b735697382cb4e811ba56740bd1cb62eac1f7d9
Lara_Soto <sotolara31@gmail.com>
2022-05-11 15:52:52 -0300

    primer commit

---
+++ src/main/java/com/catalogo/catalogoropa/CatalogoRopaApplication.java
@@ -0,0 +1,13 @@
+package com.catalogo.catalogoropa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoRopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoRopaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalogo/catalogoropa/CatalogoRopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogo.catalogoropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoRopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
